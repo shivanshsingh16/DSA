276d4d11dc21e84470475fbc48a61dafc5803083
shivanshsingh16 <singhshivansh160@gmail.com>
2026-02-11 23:05:06 +0530

    Time: 46 ms (9.13%) | Memory: 88.6 MB (17.25%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+
+        map<int,int> mpp;
+        mpp[0]=-1;
+        int cnt=0;
+        int result=0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i]==0)
+            {cnt--;
+            }
+            else{
+                cnt++;
+            }
+            if (mpp.find(cnt)!=mpp.end())
+            {
+                result=max(result,i-mpp[cnt]);
+            }
+            else{            mpp[cnt]=i;
+            }
+            
+        }
+        return result;
+        
+
+    
+    }
+};

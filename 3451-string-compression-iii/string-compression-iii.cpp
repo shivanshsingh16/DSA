58ff517c973ad9466aa0af36761af68a948e2a98
shivanshsingh16 <singhshivansh160@gmail.com>
2026-02-17 23:38:33 +0530

    Time: 27 ms (62.97%) | Memory: 28.2 MB (89.09%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        if(word.size()==0){
+            return "";
+        }
+        string comp="";
+        int i =0;
+        int cnt=0;
+        char cmp=word[0];
+        while (i<word.size())
+        {
+            if (cmp==word[i])
+            {
+                cnt++;
+                if (cnt>9)
+                {
+                    comp.push_back('9');
+                    comp.push_back(cmp);
+                    cnt=1;
+                }
+                i++;
+            }
+            else{
+                comp.push_back(cnt+'0');
+                comp.push_back(cmp);
+                cmp=word[i];cnt=0;
+            }
+        }
+        
+                comp.push_back(cnt+'0');
+                comp.push_back(cmp);
+        return comp;
+        
+    }
+};

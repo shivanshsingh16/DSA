ef02aa51ef1551f3c71a5ac34c734d75a696317f
shivanshsingh16 <singhshivansh160@gmail.com>
2026-02-23 17:57:00 +0530

    Time: 4 ms (44.40%) | Memory: 23.4 MB (65.67%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,30 @@
+class MinStack {
+public:
+    stack<pair<int,int>> stk;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if (stk.empty())
+        {
+            stk.push({val,val});
+        }
+        else{
+            stk.push({val,min(val, stk.top().second)});
+        }
+        
+    }
+    
+    void pop() {
+        stk.pop();
+    }
+    
+    int top() {
+        return stk.top().first;
+    }
+    
+    int getMin() {
+        return stk.top().second;
+    }
+};

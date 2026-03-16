09debd20bdaed3c4f6d912f92cb9a0fe06fc2123
shivanshsingh16 <singhshivansh160@gmail.com>
2026-03-16 23:44:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (58.72%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> stk;
+        int result=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                stk.push(s[i]);
+            }
+            else{
+                if(!stk.empty()&&stk.top()=='('){
+                    stk.pop();
+                }
+                else{
+                    stk.push(s[i]);
+                }
+            }
+        }
+        return stk.size();
+    }
+};

026326cbf8f9bf80e73d97ccc8b858450a574660
shivanshsingh16 <singhshivansh160@gmail.com>
2026-03-21 23:09:39 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (63.92%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i=1;
+        while(i<nums.size()){
+            if(nums[i]==nums[i-1]){
+                i=i+3;
+            }
+            else{
+                return nums[i-1];
+            }
+        }
+        return nums[nums.size()-1];
+
+    }
+};

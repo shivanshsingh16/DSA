d931931c245e6fd830bc2dc9e2cc621e4e1e1f4f
shivanshsingh16 <singhshivansh160@gmail.com>
2026-03-30 18:56:54 +0530

    Time: 9 ms (52.30%) | Memory: 19.8 MB (68.97%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
-
     bool checkStrings(string s1, string s2) {
         vector<int> arr1(26,0);
-        vector<int> arr2(26,0);
         vector<int> arr3(26,0);
-        vector<int> arr4(26,0);
         int i=0;
         while (i<s1.size())
         {
@@ -14,19 +11,35 @@ public:
                 arr1[s1[i]-'a']++;
                 arr3[s2[i]-'a']++;
             }
-            else{
+            
+            i++;
+        }
+        for (int i = 0; i < 26; i++)
+        {
+            if (arr1[i]!=arr3[i])
+            {
+                return false;
+            }
+        }
+ i =0;
+        vector<int> arr4(26,0);
+        vector<int> arr2(26,0);
+        while (i<s1.size())
+        {
+            if (i%2!=0){
                 arr2[s1[i]-'a']++;
                 arr4[s2[i]-'a']++;
             }
             i++;
         }
-        for (int i = 0; i < 26; i++)
+        for ( i = 0; i < 26; i++)
         {
-            if (arr1[i]!=arr3[i]||arr2[i]!=arr4[i])
+            if (arr2[i]!=arr4[i])
             {
                 return false;
             }
         }
+        
         return true;
     }
 };

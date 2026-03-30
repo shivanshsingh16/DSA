dda1f524279c218a48b0014bf832191115855874
shivanshsingh16 <singhshivansh160@gmail.com>
2026-03-30 19:05:12 +0530

    Time: 3 ms (92.53%) | Memory: 19.7 MB (77.59%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -2,44 +2,28 @@ class Solution {
 public:
     bool checkStrings(string s1, string s2) {
         vector<int> arr1(26,0);
-        vector<int> arr3(26,0);
+        vector<int> arr2(26,0);
         int i=0;
         while (i<s1.size())
         {
             if (i%2==0)
             {
                 arr1[s1[i]-'a']++;
-                arr3[s2[i]-'a']++;
+                arr1[s2[i]-'a']--;
             }
-            
+            else{
+                arr2[s1[i]-'a']++;
+                arr2[s2[i]-'a']--;
+            }
             i++;
         }
         for (int i = 0; i < 26; i++)
         {
-            if (arr1[i]!=arr3[i])
+            if (arr1[i]!=0||arr2[i]!=0)
             {
                 return false;
             }
         }
- i =0;
-        vector<int> arr4(26,0);
-        vector<int> arr2(26,0);
-        while (i<s1.size())
-        {
-            if (i%2!=0){
-                arr2[s1[i]-'a']++;
-                arr4[s2[i]-'a']++;
-            }
-            i++;
-        }
-        for ( i = 0; i < 26; i++)
-        {
-            if (arr2[i]!=arr4[i])
-            {
-                return false;
-            }
-        }
-        
         return true;
     }
 };

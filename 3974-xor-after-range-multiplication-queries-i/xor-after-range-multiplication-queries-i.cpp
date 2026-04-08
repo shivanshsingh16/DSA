be60c24336faf956b219859297f7449b5ae1cd88
shivanshsingh16 <singhshivansh160@gmail.com>
2026-04-08 16:03:13 +0530

    Time: 79 ms (36.36%) | Memory: 158.1 MB (49.28%) - LeetSync

--- 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.cpp
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.cpp
@@ -15,7 +15,7 @@ public:
         int result=0;
         for (int i = 0; i < nums.size(); i++)
         {
-            result=result^nums[i];
+            result^=nums[i];
         }
         
         return result;

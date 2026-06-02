891c20e8a88960bf1921833924b4b719191f4ed1
shivanshsingh16 <singhshivansh160@gmail.com>
2026-06-02 20:15:24 +0530

    Time: 7 ms (54.43%) | Memory: 91.8 MB (59.92%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -3,6 +3,7 @@ public:
     int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
         int result=INT_MAX;
         int minLand=INT_MAX;
+        int minWater=INT_MAX;
         for (int i = 0; i < landStartTime.size(); i++)
         {
             minLand=min(minLand,(landStartTime[i]+landDuration[i]));
@@ -11,14 +12,14 @@ public:
         {
             result=min(result,(max(waterStartTime[i],minLand)+waterDuration[i]));
         }
-        minLand=INT_MAX;
+        
         for (int i = 0; i < waterStartTime.size(); i++)
         {
-            minLand=min(minLand,(waterStartTime[i]+waterDuration[i]));
+            minWater=min(minWater,(waterStartTime[i]+waterDuration[i]));
         }
         for (int i = 0; i < landStartTime.size(); i++)
         {
-            result=min(result,(max(landStartTime[i],minLand)+landDuration[i]));
+            result=min(result,(max(landStartTime[i],minWater)+landDuration[i]));
         }
         return result;
         

47e8e01b54d09431af74e41eabd82e53f0214559
shivanshsingh16 <singhshivansh160@gmail.com>
2026-06-04 11:44:51 +0530

    Time: 7 ms (88.81%) | Memory: 8.7 MB (96.46%) - LeetSync

---
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    bool compare(int n1,int n2,int n3){
+        if (n1<n2&&n3<n2)
+        {
+            return true;
+        }
+        if (n1>n2&&n3>n2)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    int tell(int num){
+        return num%10;
+    }
+    int hello(int num){int answer=0;
+        int n1=-1;int n2=-1;int n3=-1;
+        while (num!=0)
+        {
+            int x=tell(num);
+            if (n1==-1&&n2==-1&&n3==-1)
+            {
+                n3=x;
+            }
+            else if (n1==-1&&n2==-1&&n3!=-1)
+            {n2=x;
+            }
+            else if (n2!=-1&&n1==-1)
+            {
+                n1=x;
+                if (compare(n1,n2,n3))
+                {
+                    answer++;
+                }
+            }
+            else{
+                n3=n2;
+                n2=n1;
+                n1=x;
+                if (compare(n1,n2,n3))
+                {
+                    answer++;
+                }
+            }
+            num=num/10;
+        }
+        return answer;
+    }
+    int totalWaviness(int num1, int num2) {
+        int result=0;
+        for (int i = num1; i <=num2; i++)
+        {
+            result+=hello(i);
+        }
+        return result;
+    }
+};

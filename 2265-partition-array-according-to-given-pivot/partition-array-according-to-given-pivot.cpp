df0d1a0c2eed3b8996df52c5f281b0e09e51cfaf
shivanshsingh16 <singhshivansh160@gmail.com>
2026-06-08 17:11:14 +0530

    Time: 3 ms (87.42%) | Memory: 127.5 MB (99.95%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,3 +1,8 @@
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {

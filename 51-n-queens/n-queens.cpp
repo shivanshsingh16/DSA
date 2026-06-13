a059227ba6737b25261dc8f0a80183ef7f61be36
shivanshsingh16 <singhshivansh160@gmail.com>
2026-06-13 19:01:12 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (78.14%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,57 +1,33 @@
 class Solution {
 public:
-    bool checkQ(vector<string> &dots, int row, int col, int n ){
-        for (int i = 0; i < col; i++) {
-            if (dots[row][i]=='Q')
-            {return false;
-            }
-        }
-        
-        int tempCol=col;
-        int tempRow=row;
-        while (tempRow>=0&&tempCol>=0)
-        {
-            if (dots[tempRow][tempCol]=='Q')
-            {return false;
-            }
-            tempRow--;
-            tempCol--;
-        }
-        
-        tempCol=col;
-        tempRow=row;
-        while (tempCol>=0&&tempRow<n)
-        {
-            if (dots[tempRow][tempCol]=='Q')
-            {
-                return false;
-            }
-            tempRow++;
-            tempCol--;
-        }
-        return true;
-    }
 
-    void queens(vector<vector<string>> &result,vector<string> dots, int col, int n ){
+    void QueenCheck(vector<vector<string>> &result,vector<string> &dots,vector<int> &rowCheck,vector<int> &lowerDia,vector<int> &upperDia, int col, int n ){
         if (col==n)
         {
-            result.push_back(dots);return;
+            result.push_back(dots);
+            return;
         }
-        for (int row = 0; row<n; row++)
+        for (int row = 0; row < n; row++)
         {
-                if (checkQ(dots, row, col , n ))
-                {
-                    dots[row][col]='Q';
-                    queens(result,dots, col+1,n );
-                    dots[row][col]='.';
-                }
+            if (rowCheck[row]==0&&lowerDia[row+col]==0&&upperDia[n-1+col-row]==0)
+            {   
+                dots[row][col]='Q';
+                rowCheck[row]=1;
+                lowerDia[row+col]=1;
+                upperDia[n-1+col-row]=1;
+                QueenCheck(result,dots, rowCheck,lowerDia, upperDia,col+1, n);
+                dots[row][col]='.';
+                rowCheck[row]=0;
+                lowerDia[row+col]=0;
+                upperDia[n-1+col-row]=0;
+            }
+            
             
         }
         
         
     }
-
-   vector<vector<string>> solveNQueens(int n) {
+    vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> result;
         vector<string> dots(n);
         string s(n,'.');
@@ -59,7 +35,10 @@ public:
         {
             dots[i]=s;;
         }
-        queens(result,dots , 0 , n);
+        vector<int> rowCheck(n,0);
+        vector<int> lowerDia((2*n) -1,0);
+        vector<int> upperDia((2*n)-1,0);
+        QueenCheck(result,dots, rowCheck,lowerDia,upperDia,0, n);
         return result;   
     }
 };

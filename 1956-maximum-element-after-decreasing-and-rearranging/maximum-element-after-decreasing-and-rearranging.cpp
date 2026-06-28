4792f30126e932e7996cbca4ec7e01d9dd5d5982
shivanshsingh16 <singhshivansh160@gmail.com>
2026-06-28 17:25:54 +0530

    Time: 12 ms (26.55%) | Memory: 55.3 MB (36.72%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -6,7 +6,7 @@ public:
         arr[0]=1;
         for (int i = 1; i < arr.size(); i++)
         {
-            if (abs(arr[i]-arr[i-1])>1)
+            if (arr[i]>1+arr[i-1])
             {
                 arr[i]=arr[i-1]+1;
             }
